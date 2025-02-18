ad41377016f52d94d068d20c7dfb452a98d15a05
MohamedAyman2012000 <ma2012000@gmail.com>
2025-02-18 18:00:57 +0200

    Add NavBar Icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TestComponent } from './test/test.component';
 import { CategoriesComponent } from './components/categories/categories/categories.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CategoriesComponent, TestComponent],
+  imports: [CategoriesComponent, TestComponent,NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
