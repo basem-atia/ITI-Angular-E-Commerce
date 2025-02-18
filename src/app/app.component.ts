c0d9c16ef887ce3a2460ff22f694825a87c33bf9
basem-atia <basematiaelsayed@gmail.com>
2025-02-18 17:03:05 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TestComponent } from './test/test.component';
+import { CategoriesComponent } from './components/categories/categories/categories.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [CategoriesComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'Project';
+}
