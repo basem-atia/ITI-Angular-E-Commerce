d6f1e7f205d4f2f3c8a6b5a4a214cac9a86fdcdf
karim mohamed <karimhelmi4@mail.com>
2025-02-22 18:46:45 +0200

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { TestComponent } from './test/test.component';
 import { CategoriesComponent } from './components/categories/categories/categories.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CategoriesComponent, TestComponent ,SideBarComponent],
+  imports: [
+    CategoriesComponent,
+    TestComponent,
+    SideBarComponent,
+    RegisterComponent,
+    LoginComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
