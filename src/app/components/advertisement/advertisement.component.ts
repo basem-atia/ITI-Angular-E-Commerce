3adcc4aa235a1f6f6cc2d0647af8e807ec83bdc5
MohamedEl-Tabei <mohamedeltabei2000@gmail.com>
2025-03-02 16:41:50 +0200

    Products

--- src/app/components/advertisement/advertisement.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-advertisement',
-  imports: [],
-  templateUrl: './advertisement.component.html',
-  styleUrl: './advertisement.component.css',
-})
-export class AdvertisementComponent {}
